8de95a7af06ab1e5d877176a432d341eb703b306
RubenArthuys <ruben.arthuys@gmail.com>
2020-10-05 11:31:01 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList'
+import uuidv4 from 'uuid/v4'
+
 
 function App() {
+  //destructured variables, which are = to result of useState
+  const [todoVariable, setTodos] = useState([])
+  //get what's written in input with useRef Hook, to send to the function below to add a 'todo'
+  const todoNameRef = useRef()
+  const LOCAL_STORAGE_KEY = 'todoApp.todoVariable'
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todoVariable))
+  }, [todoVariable])
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      //spread operator for all the previous todos, + a new Todo
+      //uuid library to create random ids with import above
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })    
+    todoNameRef.current.value = null
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <TodoList todoProp={todoVariable} />
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo} >Add Todo</button>
+    <button>Clear Completed</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
